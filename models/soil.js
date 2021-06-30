7954788f23dc5afb8f10c85ae8ad1efa62258d21
Suhara Vithanage <suha.lyoko@hotmail.com>
2021-06-30 11:12:06 +0530

    changed the model

--- models/soil.js
+++ models/soil.js
@@ -4,7 +4,11 @@ const soilSchema = new mongoose.Schema({
   sampleID: { type: String, required: true },
   location: { type: String, required: true },
   address: { type: String, required: true },
-  spectralData: [{ type: String, required: true }],
+  spectralData: [
+    { 
+      name: {type: String, required: true}
+     }
+  ],
   imageData: {
     cameraImage: { type: String },
     microscopicImage: { type: String },
